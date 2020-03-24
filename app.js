ce58d31367125de032e7e2c64e0b43b47918b612
steliosk9 <steliosk9@gmail.com>
2020-03-24 17:28:02 -0500

    start the game function

--- app.js
+++ app.js
@@ -11,54 +11,30 @@ GAME RULES:
 
 var scores, roundScore, activePlayer, gamePlaying;
 
-// init();
-// var lastDice;
 
 scores = [0,0];
 roundScore = 0;
 activePlayer = 1;
 
-document.querySelector('.btn-roll').addEventListener('click', function() {
-    if(gamePlaying) {
-        // 1. Random number
-        var dice1 = Math.floor(Math.random() * 6) + 1;
-        var dice2 = Math.floor(Math.random() * 6) + 1;
 
-        //2. Display the result
-        document.getElementById('dice-1').style.display = 'block';
-        document.getElementById('dice-2').style.display = 'block';
-        document.getElementById('dice-1').src = 'dice-' + dice1 + '.png';
-        document.getElementById('dice-2').src = 'dice-' + dice2 + '.png';
+document.querySelector('.dice').style.display = 'none';
 
-        //3. Update the round score IF the rolled number was NOT a 1
-        if (dice1 !== 1 && dice2 !== 1) {
-            //Add score
-            roundScore += dice1 + dice2;
-              // Alternative we can manipulate the html with innerhtml instead textContent
-             //  Add css with document.querySelector('.dice').style.display = 'none';    
-            document.querySelector('#current-' + activePlayer).textContent = roundScore;
-          
-        } else {
-            //Next player
-            nextPlayer();
-        }
-        
-        /*
-        if (dice === 6 && lastDice === 6) {
-            //Player looses score
-            scores[activePlayer] = 0;
-            document.querySelector('#score-' + activePlayer).textContent = '0';
-            nextPlayer();
-        } else if (dice !== 1) {
-            //Add score
-            roundScore += dice;
-            document.querySelector('#current-' + activePlayer).textContent = roundScore;
-        } else {
-            //Next player
-            nextPlayer();
-        }
-        lastDice = dice;
-        */
-    }    
+
+  //2. Display the result
+  document.getElementById('score-0').textContent = '0'
+  document.getElementById('score-1').textContent = '0'
+  document.getElementById('current-0').textContent = '0'
+  document.getElementById('current-1').textContent = '0'
+
+// Start the game
+
+document.querySelector('.btn-roll').addEventListener("click", function() {
+    var dice = Math.floor(Math.random() * 6) + 1;
+
+    // display the result
+    var diceDom = document.querySelector('.dice');
+    diceDom.style.display = 'block';
+    diceDom.src = './images/dice-' + dice + '.png';
+
+
 });
-
