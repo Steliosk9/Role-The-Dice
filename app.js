d66e36f350db3ecb66dd48cc93896ad1a23d5c0d
steliosk9 <steliosk9@gmail.com>
2020-03-31 20:04:44 -0500

    remove dice and active-player class  after a player wins

--- app.js
+++ app.js
@@ -55,6 +55,9 @@ document.querySelector('#score-' + activePlayer).textContent = scores[activePlay
 
     if(scores[activePlayer] >= 20){
   document.querySelector('#name-' + activePlayer).textContent = 'Winner!!!';
+  document.querySelector('.dice').style.display='none';
+  document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner')
+  document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active')
     } else {
     nextPlayer()
     }
